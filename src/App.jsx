59dcd21db6ffc7191d19ef31ef284b7a8a58a55e
Kage <dumitruwebdev@gmail.com>
2024-09-03 16:24:26 +0300

    update component home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import CorporateIdentity from './pages/NewsPage';
 
 function App() {
