3d12f3c9c84e2042874acf44afde89561e483aa4
Kage <dumitruwebdev@gmail.com>
2024-09-08 17:29:49 +0300

    update

--- src/components/BtnGetStarted.jsx
+++ src/components/BtnGetStarted.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react";
+import { UtilsContext } from "../Context/ContextProvider";
+
 const BtnGetStarted = () => {
-  return <button className='bg-green-500 text-white py-2 px-7 text-xl rounded-md pb-3'>Get Started</button>
+  const { setIsModalOpenContact } = useContext(UtilsContext);
+
+  return <button onClick={() => setIsModalOpenContact(true)} className='bg-green-500 text-white py-2 px-7 text-xl rounded-md pb-3'>Get Started</button>
 }
 
 export default BtnGetStarted;
